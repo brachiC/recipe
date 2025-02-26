7db5cf12f9dfbb2f0a85ba81de24af1e1fa05717
ברכה חשין <BrachaCh@smg.local>
2025-02-26 19:33:51 +0200

    getAllRecipes

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -3,3 +3,13 @@ exports.addRecipe = async(req,res)=>{
     const recipe = await Recipe.create(req.body);
     res.json(user)
 }
+exports.getAllRecipes = async (req, res) => {
+  try {
+
+    const recipes = await Recipe.find();
+    res.json(recipes);
+  } catch (error) {
+    console.error('Failed to get recipes:', error);
+    res.status(500).json({ message: 'Failed to get recipes' });
+  }
+};
