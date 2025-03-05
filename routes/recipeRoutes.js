85e2314f895ef0ad385f9341aee96b4f7d927dfd
ברכה חשין <BrachaCh@smg.local>
2025-03-05 20:50:20 +0200

    getRecipeById

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,8 +1,9 @@
 const express=require('express');
 const router=express.Router();
 
-const {addRecipe, deleteRecipe, updateRecipe,getAllRecipes}=require('../controllers/recipeController');
+const {addRecipe, deleteRecipe,getRecipeById, updateRecipe,getAllRecipes}=require('../controllers/recipeController');
 router.post('/',addRecipe);
 router.get('/',getAllRecipes);
 router.delete('/:_id',deleteRecipe);
+router.get('/:_id',getRecipeById);
 module.exports=router;
