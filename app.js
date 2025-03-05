f6e7b10205ce92b668d921cf75850fa89b30d2e1
ברכה חשין <BrachaCh@smg.local>
2025-03-05 20:24:54 +0200

    deleteRecipe

--- app.js
+++ app.js
@@ -12,31 +12,37 @@
 // app.get('/', (req, res) => {
 //     res.send('Data received successfully');
 // });
-
+ 
 
 // app.listen(8000, () => {
 //     console.log("port 8000");
 // });
-const mongoose=require("mongoose")
 require("dotenv").config()
+const mongoose=require("mongoose")
 const express = require("express")
 const cors = require("cors")
 const corsOptions = require("./config/corsOptions")
 const connectDB = require("./config/dbConn")
+const recipeRoutes =require('./routes/recipeRoutes')
+
 const PORT = process.env.PORT || 7001
+// require("dotenv").config()
 const app = express()
+
+
+
+
 connectDB()
 //middlewares
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.static("public"))
+app.use('/', recipeRoutes);
 //routes
 app.get("/",(req,res)=>{
 res.send("this is the home page")
+
 })
-// app.listen(PORT, ()=>{
-// console.log(`Server running on port ${PORT}`)
-// })
 
 mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB')
--- models/Recipe.js
+++ models/Recipe.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const RecipesSchema=new mongoose.Schema({
     nameRecipe:String,
     categoryRecipe:String,
-    interduction:[] //?
+    interduction:Array,
 })
 
 module.exports=mongoose.model('recipe',RecipesSchema)
